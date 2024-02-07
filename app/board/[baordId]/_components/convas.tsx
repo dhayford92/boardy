f425bdaf62f748410fba2072ba6ea45a848df7d7
Denzel Hayford <dhayford@gmail.com>
2024-02-07 02:17:47 -1200

    Board Detail Added

---
+++ app/board/[baordId]/_components/convas.tsx
@@ -0,0 +1,18 @@
+'use client'
+
+import React from 'react'
+import Info from './info'
+import Participants from './paticipants'
+import Toolbar from './toolbar'
+
+function Canvas() {
+  return (
+    <main className='h-full w-full relative bg-neutral-100 touch-none'>
+        <Info/>
+        <Participants/>
+        <Toolbar/>
+    </main>
+  )
+}
+
+export default Canvas
---
+++ app/board/[baordId]/page.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import Canvas from './_components/convas'
+
+export default function BoardIdPage() {
+  return (
+    <Canvas/>
+  )
+}
