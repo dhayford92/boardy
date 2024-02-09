e776edd46f8dcee514d56fb9ec5c30265efd0f56
Denzel Hayford <dhayford@gmail.com>
2024-02-09 02:49:54 -1200

    Toolbar action added

--- app/board/[boardId]/_components/convas.tsx
+++ app/board/[boardId]/_components/convas.tsx
@@ -1,20 +1,38 @@
 'use client'
 
-import React from 'react'
+import React, { useState } from 'react'
 import Info from './info'
 import Participants from './paticipants'
 import Toolbar from './toolbar'
+import { CanvasMode, CanvasState } from '@/types/canvas'
+import { useCanRedo, useCanUndo, useHistory } from '@/liveblocks.config'
 
 
-function Canvas({boardId}:{boardId: string}) {
+function Canvas({
+  boardId}:{
+    boardId: string
+  }) {
 
- 
+    const [canvasState, setCanvasState] = useState<CanvasState>({
+      mode: CanvasMode.None,
+    })
+
+    const history = useHistory();
+    const canUndo = useCanUndo();
+    const canRedo = useCanRedo();
 
   return (
     <main className='h-full w-full relative bg-neutral-100 touch-none'>
         <Info boardId={boardId}/>
         <Participants/>
-        <Toolbar/>
+        <Toolbar
+          canvasState={canvasState}
+          setCanvasState={setCanvasState}
+          canRedo={canRedo}
+          canUndo={canUndo}
+          undo={history.undo}
+          redo={history.redo}
+        />
     </main>
   )
 }
